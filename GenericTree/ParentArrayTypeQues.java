2f12b8198085f5c4e464586cbf8fa5260620eaed
abhilash <getabhilashp@gmail.com>
2021-10-02 16:22:14 +0530

    done

--- GenericTree/ParentArrayTypeQues.java
+++ GenericTree/ParentArrayTypeQues.java
@@ -1,4 +1,6 @@
-
+//Given a parent arrray P, where P[i] indicates the parent of ith node in the tree
+// O(n*n) solution
+// if we carefully observe we just need to start at every node and keep going to its parent node untillwe reach -1
 
 package GenericTree;
 import java.util.*;
@@ -16,9 +18,29 @@ public class ParentArrayTypeQues {
             array[i] = sc.nextInt();
         }
 
-        parentHight(int[] arr);
+        System.out.println(parentHight(array));
 
 
+
+    }
+
+    public static int parentHight(int[] arr) 
+    {
+        int maxDepth = -1, CurrentDepth =-1, j;
+        for( int i =0 ; i < arr.length; i++)
+        {
+            CurrentDepth = 0; j =i;
+            while( arr[j] != -1)
+            {
+                CurrentDepth++ ;
+                j = arr[j];
+            }
+            if(CurrentDepth > maxDepth)
+            {
+                maxDepth = CurrentDepth;
+            }
+        }
+        return maxDepth;
     }
 
 }
