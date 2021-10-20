2d5d013ca1cf2a95f15488a72e7db7a3827a5561
abhilash <getabhilashp@gmail.com>
2021-10-21 00:41:47 +0530

    commit Infinite Array

---
+++ DivideandConquer/InfiniteArray.java
@@ -0,0 +1,11 @@
+//give an infinite array in which the first n cell contains integer in sorted order and rest of filled with some special
+//charachter . Assume we do not know n value. Give an algorithm that takes integer k as input and find a position 
+//in array containing K, if such position exist in  O(log n) time.
+
+package DivideandConquer;
+
+public class InfiniteArray {
+
+    
+    
+}
